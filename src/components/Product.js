372507c63011c4e1bc4dc17c071f777df5f2cba6
Oliver Spring <oliver9589@gmail.com>
2017-09-17 05:10:55 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { createPropType } from './../../lib/custom-proptypes';
 
 class Product extends React.Component {
   render() {
