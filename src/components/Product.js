861c19bb29c9a398a4a08e87aa2c47c8d294a52a
Oliver Spring <oliver9589@gmail.com>
2017-09-17 04:26:44 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,15 @@
-// Code Product Component Here
+import React from 'react';
+
+class Product implements React.component {
+  render() {
+    return (
+      <ul>
+        <li> {this.props.name} </li>
+        <li> {this.props.producer} </li>
+        <li> {this.props.hasWatermark} </li>
+        <li> {this.props.color} </li>
+        <li> {this.props.weight} </li>
+      </ul>
+    )
+  }
+}
