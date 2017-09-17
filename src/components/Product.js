9c623b77324b51efc9cb202d184781d2fe2cfb43
Oliver Spring <oliver9589@gmail.com>
2017-09-17 05:30:50 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -40,9 +40,10 @@ Product.propTypes = {
   hasWatermark: PropTypes.bool,
   color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
   weight: function(props, propName, componentName) {
-    // if (!props[propName]) {
-    //   return new Error();
-    // }
+    console.log(props[propName])
+    if (!props[propName]) {
+      return new Error();
+    }
     if (typeof props[propName] !== 'number') {
       return new Error();
     }
