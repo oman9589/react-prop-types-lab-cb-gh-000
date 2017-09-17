4a87d3d374942b27f26e141dc339d9ff912ebd55
Oliver Spring <oliver9589@gmail.com>
2017-09-17 05:16:16 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -22,7 +22,7 @@ Product.defaultProps = {
 }
 
 function weightRange(props, propName, componentName) {
-  if (!props[propName]) {
+  if (props[propName]) {
     let value = props[propName];
 
     // If no hidden label is set, children are required for accessibility
