eb0657e4cdc966c84ebc8f64085796716f7e5a8c
Oliver Spring <oliver9589@gmail.com>
2017-09-17 04:31:59 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,13 +4,15 @@ import PropTypes from 'prop-types';
 class Product extends React.component {
   render() {
     return (
-      <ul>
-        <li> {this.props.name} </li>
-        <li> {this.props.producer} </li>
-        <li> {this.props.hasWatermark} </li>
-        <li> {this.props.color} </li>
-        <li> {this.props.weight} </li>
-      </ul>
+      <div>
+        <ul>
+          <li> {this.props.name} </li>
+          <li> {this.props.producer} </li>
+          <li> {this.props.hasWatermark} </li>
+          <li> {this.props.color} </li>
+          <li> {this.props.weight} </li>
+        </ul>
+      </div>
     )
   }
 }
