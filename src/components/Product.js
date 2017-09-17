3d24b809f75bfa66ef3fe529237cedae76652e50
Oliver Spring <oliver9589@gmail.com>
2017-09-17 04:29:36 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class Product inherits React.component {
+class Product extends React.component {
   render() {
     return (
       <ul>
