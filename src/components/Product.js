eaa8ca41cbfda034a2ecf77a1e61f21dbab4091b
Oliver Spring <oliver9589@gmail.com>
2017-09-17 04:59:02 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -23,7 +23,7 @@ Product.defaultProps = {
 
 function weightRange(props, propName, componentName) {
   let value = props[propName];
-  if (typeof value === 'number') {
+  if (value % 1 === 0) {
     if (value < 80 || value > 300) {
       return new Error(propName + ' in ' + componentName + " is longer not within weight limit");
     }
